f0b59c1c5d940fbd7570581dbb1e6ad7d5790ec8
Jeff <jeff.sagal@briteweb.com>
2019-09-13 12:48:37 -0700

    add tests

---
+++ tests/unit/OptionKey.spec.js
@@ -0,0 +1,25 @@
+import Select from '../../src/components/Select.vue';
+
+describe('Serializing Option Keys', () => {
+
+  const getOptionKey = Select.props.getOptionKey.default;
+
+  it('can serialize strings to a key', () => {
+    expect(getOptionKey('vue')).toBe('"vue"');
+  });
+
+  it('can serialize integers to a key', () => {
+    expect(getOptionKey(1)).toBe('1');
+  });
+
+  it('can serialize objects to a key', () => {
+    expect(getOptionKey({label: 'vue'})).toBe('{"label":"vue"}');
+  });
+
+  it('will use an ID property if the object contains one', () => {
+    expect(getOptionKey({id: 1})).toBe(1);
+    expect(getOptionKey({id: 'one'})).toBe('one');
+    expect(getOptionKey({id: {im: 'a nested object'}}))
+      .toEqual({im: 'a nested object'});
+  });
+});
