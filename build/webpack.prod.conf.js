e7103fc5bd0c267cfd71dbdec0baf213d8e1da3f
Jeff <jeff.sagal@briteweb.com>
2019-11-07 19:33:23 -0800

    don't drop console when minify

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -14,11 +14,6 @@ module.exports = merge(baseWebpackConfig, {
     minimizer: [
       new TerserPlugin({
         sourceMap: true,
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
-        },
       }),
     ],
   }
