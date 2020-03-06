75b9d12c092fd1193b914d1ba13b73f394ab3c43
Jeff <sagalbot@gmail.com>
2020-03-06 08:08:28 -0800

    Update InternationalText.spec.js

--- tests/unit/InternationalText.spec.js
+++ tests/unit/InternationalText.spec.js
@@ -1,21 +1,22 @@
 import { mountDefault } from '../helpers';
 
-fdescribe('International Text', () => {
+describe('International Text', () => {
 
   it('renders default text for spinner.text', () => {
     const Select = mountDefault({loading: true,});
     expect(Select.find('.vs__spinner').text()).toBe('Loading...')
   });
 
-  it('renders default text for noOptions.text', () => {
+  it('renders default text for noOptions.text', async () => {
     const Select = mountDefault({options: []});
     Select.vm.open = true;
+    await Select.vm.$nextTick();
     expect(Select.find('.vs__no-options').text()).toBe('Sorry, no matching options.')
   });
 
   it('renders default text for search.ariaLabel', () => {
     const Select = mountDefault();
-    expect(Select.find('.vs__search').attributes()['aria-label']).toBe('Search for options')
+    expect(Select.find('[role="combobox"]').attributes()['aria-label']).toBe('Search for options')
   });
 
   it('renders default text for deselect.ariaLabel', () => {
