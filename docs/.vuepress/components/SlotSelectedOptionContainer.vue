a820f06d61e7100402b95fad5d2ed1c012cbae80
Jeff Sagal <sagalbot@gmail.com>
2020-03-11 09:16:07 -0700

    docs: overhaul slot docs (#1099)
    * docs: sort slots alphabetically

---
+++ docs/.vuepress/components/SlotSelectedOptionContainer.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-select :options="books" label="title">
+    <template #selected-option-container="{ option, deselect, multiple, disabled }">
+      <div class="vs__selected">{{ option.title }}</div>
+    </template>
+  </v-select>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    books: [
+      {
+        title: "Old Man's War",
+        author: {
+          firstName: "John",
+          lastName: "Scalzi"
+        }
+      }
+    ]
+  })
+}
+</script>
---
+++ docs/.vuepress/components/SlotSpinner.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-select :loading="true">
+    <template #spinner="{ loading }">
+      <div v-if="loading" style="border-left-color: rgba(88,151,251,0.71)" class="vs__spinner">
+        The .vs__spinner class will hide the text for me.
+      </div>
+    </template>
+  </v-select>
+</template>
