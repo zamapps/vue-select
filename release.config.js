bb3f28478fc6816e975a3661667a79d4b9ed8c49
Jeff Sagal <sagalbot@gmail.com>
2020-03-12 20:44:14 -0700

    ci: semantic release plugin config (#1102)

--- release.config.js
+++ release.config.js
@@ -6,9 +6,14 @@ module.exports = {
     "@semantic-release/npm",
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/github", {
-      "assets": ["dist/**"]
-    }],
+    [
+      "@semantic-release/github",
+      {
+        assets: ["dist/**"],
+        successComment:
+          ":tada: This issue has been resolved in version ${nextRelease.version} :tada:\\n\\nThe release is available on [GitHub release](<github_release_url>)\\n\\nPlease consider [sponsoring Vue Select](https://github.com/sponsors/sagalbot), your support is much appreciated! :+1:"
+      }
+    ],
     [
       "@semantic-release/git",
       {
