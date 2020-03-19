fea430df2731a2d1eaf6a020bfb16b75b268bcdc
Hector Romero <tochoromero@users.noreply.github.com>
2020-03-19 10:12:32 -0700

    fix: use width from getBoundingClientRect as the default for calculatePosition (#1114)
    * fix: use box-sizing: border-box; on dropdown menu
    
    Co-authored-by: Jeff <sagalbot@gmail.com>

--- src/directives/appendToBody.js
+++ src/directives/appendToBody.js
@@ -1,10 +1,10 @@
 export default {
     inserted (el, bindings, {context}) {
         if (context.appendToBody) {
-            const {height, top, left} = context.$refs.toggle.getBoundingClientRect();
+            const {height, top, left, width} = context.$refs.toggle.getBoundingClientRect();
 
             el.unbindPosition = context.calculatePosition(el, context, {
-                width: context.$refs.toggle.clientWidth + 'px',
+                width: width + 'px',
                 top: (window.scrollY + top + height) + 'px',
                 left: (window.scrollX + left) + 'px',
             });
