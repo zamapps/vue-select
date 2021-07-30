fc9ad2b595f8afa7520ca600aa5611d96f3ff58a
Jeff Sagal <sagalbot@gmail.com>
2021-07-30 09:28:47 -0700

    Update index.js

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,5 +1,6 @@
 import ajax from './ajax'
+import i18n from './i18n.js';
 import pointer from './typeAheadPointer'
 import pointerScroll from './pointerScroll'
 
-export default { ajax, pointer, pointerScroll }
+export default { ajax, pointer, pointerScroll, i18n }
