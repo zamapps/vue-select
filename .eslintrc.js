0837887560d9164146a793c6edd2935383c83827
Jeff Sagal <sagalbot@gmail.com>
2021-07-30 08:16:44 -0700

    update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,10 @@
 module.exports = {
+  parser: '@babel/eslint-parser',
   plugins: ['prettier'],
   extends: [
     'plugin:prettier/recommended',
     'plugin:vue/recommended',
     'prettier/vue'
   ],
-  ignorePatterns: ['node_modules/', 'dist/', 'coverage/'],
-  rules: {
-    semi: ['error', 'always'],
-    quotes: ['error', 'single'],
-    // indent: ['error', 2],
-    'no-console': 'off',
-    'no-debugger': 'error',
-    'prettier/prettier': 'error',
-    'vue/html-self-closing': 'off',
-    'vue/max-attributes-per-line': 'off'
-  }
-};
+  ignorePatterns: ['node_modules/', 'dist/', 'coverage/']
+}
