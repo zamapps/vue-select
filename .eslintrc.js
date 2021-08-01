69d11c7f58969d3bd658112246471c410564cd54
Jeff Sagal <sagalbot@gmail.com>
2021-08-01 11:42:38 -0700

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,21 @@ module.exports = {
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: 'babel-eslint',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['prettier'],
   extends: [
     'plugin:prettier/recommended',
     'plugin:vue/recommended',
-    'prettier/vue'
+    'prettier/vue',
   ],
-  ignorePatterns: ['node_modules/', 'dist/', 'coverage/']
+  ignorePatterns: [
+    '!.*.js',
+    '!docs/.vuepress',
+    'docs/.vuepress/dist',
+    'node_modules',
+    'dist',
+    'coverage',
+    'yarn.lock',
+  ],
 }
