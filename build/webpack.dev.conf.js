f8a41df594eb1911247cc5de05f037c29157f416
Jeff Sagal <sagalbot@gmail.com>
2022-02-18 12:49:42 -0800

    build: fix webpack dev environment (#1593)

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,36 +1,21 @@
 const { merge } = require('webpack-merge')
-const chokidar = require('chokidar')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const baseWebpackConfig = require('./webpack.base.conf')
 
-/**
- *
- */
 module.exports = merge(baseWebpackConfig, {
   entry: './dev/dev.js',
+  devServer: {
+    open: true,
+    static: false,
+    client: { overlay: true },
+    watchFiles: ['dev/dev.html'],
+  },
+  stats: false,
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'dev',
       template: './dev/dev.html',
       inject: true,
     }),
   ],
-  optimization: {
-    noEmitOnErrors: true,
-  },
-  devServer: {
-    hot: 'only',
-    open: true,
-    // inline: true,
-    // stats: {
-    //   children: false,
-    //   modules: false,
-    //   chunks: false,
-    // },
-    port: 8080,
-    // onBeforeSetupMiddleware(server) {
-    //   chokidar.watch(['./**/*.html']).on('all', function () {
-    //     server.sockWrite(server.sockets, 'content-changed')
-    //   })
-    // },
-  },
 })
